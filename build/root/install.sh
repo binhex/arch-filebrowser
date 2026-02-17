50aaa0029da02c98eecb1b9fbc8141295169b925
binhex <megalith01@gmail.com>
2026-02-17 13:21:12 +0000

    switch to not required, as we have defaults

--- build/root/install.sh
+++ build/root/install.sh
@@ -98,12 +98,11 @@ source utils.sh
 # Define environment variables to process
 # Format: "VAR_NAME:DEFAULT_VALUE:REQUIRED:MASK"
 env_vars=(
-	"FILEBROWSER_USERNAME:admin:true:false"
-	"FILEBROWSER_PASSWORD:filebrowser:true:true"
-	"FILEBROWSER_BASEURL:/:true:false"
-	"FILEBROWSER_ROOT:/media:true:false"
+	"FILEBROWSER_USERNAME:admin:false:false"
+	"FILEBROWSER_PASSWORD:filebrowser:false:true"
+	"FILEBROWSER_BASEURL:/:false:false"
+	"FILEBROWSER_ROOT:/media:false:false"
 	"ENABLE_TLS:no:false:false"
-
 )
 
 # Process each environment variable
